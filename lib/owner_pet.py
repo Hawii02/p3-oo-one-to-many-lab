b455494482ef1c071119952735a013d57f0f554d
leon <onditileon21@gmail.com>
2024-03-10 14:50:39 +0300

    correct on owner attribute

--- lib/owner_pet.py
+++ lib/owner_pet.py
@@ -1,5 +1,23 @@
 class Pet:
-    pass
+    PET_TYPES = ["dog", "cat", "rodent", "bird", "reptile", "exotic"]
+    all = []
+    def __init__(self, name, pet_type, owner=None):
+        self.name = name
+        self.pet_type = pet_type
+        if pet_type not in self.PET_TYPES:
+            raise Exception("Invalid pet type")
+        self.owner = owner
+        Pet.all.append(self)
 
 class Owner:
-    pass
+    def __init__(self, name):
+        self.name = name
+
+    def pet(self):
+        return [pet for pet in Pet.all if pet.owner == self]
+    
+    def get_sorted_pets(self, pet):
+        if not isinstance(pet, Pet):
+            raise TypeError("Pet must be an instance of Pet class")
+        pet.owner = self
+        
